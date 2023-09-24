1557589a15fa5ba37f0b4c816529c749ab769fe2
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-09-24 12:40:52 -0600

    finnished TODO's in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  const dbName = 'jate';
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Add logic to a method that accepts some content and adds it to the database
+  export const putDb = async (content) => {
+    const db = await openDB(dbName, 1);
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+  
+    const id = await store.add({ content });
+    await tx.done;
+  
+    console.log(`Added content with ID: ${id}`);
+  };
+  
+  // Add logic for a method that gets all the content from the database
+  export const getDb = async () => {
+    const db = await openDB(dbName, 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+  
+    const contentArray = await store.getAll();
+    await tx.done;
+  
+    return contentArray;
+  };
 
 initdb();
