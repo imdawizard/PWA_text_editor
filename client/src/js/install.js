2ede7b600f6fd190943fdeb2126218e5a734ae21
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-09-24 16:22:34 -0600

    messed around webpack.config.js file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -10,33 +10,35 @@
 // // TODO: Add an handler for the `appinstalled` event
 // window.addEventListener('appinstalled', (event) => {});
 
+const butInstall = document.getElementById("buttonInstall");
 
-const butInstall = document.getElementById('buttonInstall');
-
-let deferredPrompt;
-
-// Logic for installing the PWA
-// Added an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-  event.preventDefault(); // Prevent the default browser prompt
-  deferredPrompt = event;
-  butInstall.style.display = 'block'; // Display the install button
-});
 
-// Implement a click event handler on the `butInstall` element
+    // Store the triggered events
+    window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
+  });
+
 butInstall.addEventListener('click', async () => {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    const choiceResult = await deferredPrompt.userChoice;
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the installation');
-    }
-    deferredPrompt = null; // Reset the deferredPrompt
-    butInstall.style.display = 'none'; // Hide the install button
+  
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+   return;
   }
+
+  // Show prompt
+  promptEvent.prompt();
+  
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
 });
 
-// Added a handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-  console.log('App installed by the user');
+  // Clear prompt
+  window.deferredPrompt = null;
 });
